d5932d14c96c95271c9e563f9ed8c592dc525970
janina280 <coceijanina@yahoo.com>
2025-02-27 16:37:11 +0200

    add licensing

--- PixelForge/PixelForge/PixelForge.Client/Program.cs
+++ PixelForge/PixelForge/PixelForge.Client/Program.cs
@@ -2,5 +2,8 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Syncfusion.Blazor;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("MzczODI1N0AzMjM4MmUzMDJlMzBaVnRVVWwzaGFlQ2RGaEM3dFFZUnk0VXRoRVVNNXZBdVNCV2ZQTlBlTE8wPQ==");
+
 builder.Services.AddSyncfusionBlazor();
 await builder.Build().RunAsync();
